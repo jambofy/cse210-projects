9ea7c7e0976476f722f19bfe033bdd8f8e21bd7a
nyasha <mrshepard18@gmail.com>
2024-09-19 17:37:23 +0200

    edit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        bool playAgain = true;
+        Random random = new Random();
+
+        while (playAgain)
+        {
+            int magicNumber = random.Next(1, 101);
+            int guesses = 0;
+
+            Console.WriteLine("Welcome to Guess My Number game!");
+
+            while (true)
+            {
+                Console.Write("What is your guess? ");
+                string input = Console.ReadLine();
+                int guess;
+
+                {
+                    guesses++;
+                    if (guess < magicNumber)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    else if (guess > magicNumber)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You guessed it!");
+                        Console.WriteLine($"It took you {guesses} guesses.");
+                        break;
+                    }
+                }
+               
+            }
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            string response = Console.ReadLine().ToLower();
+
+            playAgain = response == "yes";
+        }
     }
 }
