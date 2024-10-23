74b591abbc63a352200ccaa56eb71312d0fed49a
jambofy <mrshepard18@gmail.com>
2024-10-23 11:06:09 +0200

    add

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -9,12 +9,12 @@ class Program
         // Create a list of Shape objects
         List<Shape> shapes = new List<Shape>();
 
-        // Add a square, rectangle, and circle to the list
+        
         shapes.Add(new Square("Red", 4));
         shapes.Add(new Rectangle("Blue", 5, 6));
         shapes.Add(new Circle("Green", 3));
 
-        // Iterate through the list and display the color and area of each shape
+        // Iterating through the list and display the color and area of each shape
         foreach (Shape shape in shapes)
         {
             Console.WriteLine($"Shape Color: {shape.GetColor()}");
